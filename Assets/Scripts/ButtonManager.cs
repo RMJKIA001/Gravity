60602d92745c44b0de60879d8c3c89fbb2353ad7
PROBIE\Kiara <rmjkia001>
2017-09-03 01:11:49 +0200

    Added Menu

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    public GameObject play;
+    public GameObject howto;
+    public GameObject settings;
+    public GameObject exit;
+    public GameObject sinlge;
+    public GameObject two;
+    public GameObject load;
+    public GameObject start;
+    public GameObject join;
+    public GameObject create;
+    public GameObject back;
+    public void Playbtn()
+    {
+         play.SetActive(false);
+         howto.SetActive(false);
+         settings.SetActive(false);
+         exit.SetActive(false);
+         sinlge.SetActive(true);
+         two.SetActive(true);
+         back.SetActive(true);
+    }
+    //Not for prototype
+    public void HowTobtn()
+    {
+
+    }
+    //Not for prototype
+    public void Settingsbtn()
+    {
+
+    }
+
+    public void Exitbtn()
+    {
+        Application.Quit();
+    }
+
+    public void SinglePlayerbtn ()
+    {
+        sinlge.SetActive(false);
+        two.SetActive(false);
+        load.SetActive(true);
+        start.SetActive(true);
+    }
+
+    public void Newbtn()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+    //Not for prototype
+    public void Loadbtn()
+    {
+
+    }
+    //Not for prototype
+    public void TwoPlayerbtn()
+    {
+        sinlge.SetActive(false);
+        two.SetActive(false);
+        join.SetActive(true);
+        create.SetActive(true);
+    }
+    //Not for prototype
+    public void Createbtn()
+    {
+
+    }
+    //Not for prototype
+    public void Joinbtn()
+    {
+
+    }
+
+    public void Backbtn()
+    {
+        if(sinlge.activeSelf)
+        {
+            play.SetActive(true);
+            howto.SetActive(true);
+            settings.SetActive(true);
+            exit.SetActive(true);
+            sinlge.SetActive(false);
+            two.SetActive(false);
+            back.SetActive(false);
+        }
+        if(join.activeSelf || start.activeSelf)
+        {
+            sinlge.SetActive(true);
+            two.SetActive(true);
+            join.SetActive(false);
+            create.SetActive(false);
+            load.SetActive(false);
+            start.SetActive(false);
+        }
+    }
+
+}
